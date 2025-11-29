44a24a16cbe5d0608f450aaf3d64f86794ced75a
nisiviloria <nisiashley22@gmail.com>
2025-11-29 21:32:43 +0800

    update config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()], 
-  base: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/my-portfolio/' : '/',
   assetsInclude: ['**/*.glb', '**/*.gltf'],
    build: {
     assetsInlineLimit: 0, 
