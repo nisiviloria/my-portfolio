2423441587bca5a67ae234221cc2bca542a237b3
nisiviloria <nisiashley22@gmail.com>
2025-12-18 19:10:53 +0800

    Updated file

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { PortfolioLanding } from "./screens/PortfolioLanding";
 import AllProjects from "./screens/Portfoliolanding/sections/Projects/AllProjects";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<PortfolioLanding />} />
         <Route path="/all-projects" element={<AllProjects />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
